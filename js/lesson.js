33d5af34a515bccda228dc48da73a09dc3542c72
cpu <shakemidinovemir@gmail.com>
2025-04-09 22:39:12 +0600

    init

--- js/lesson.js
+++ js/lesson.js
@@ -13,3 +13,50 @@ phoneButton.onclick =  () => {
         phoneResult.style.color = "red"
     }
 }
+
+
+// Tab slider
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block');
+const tabContentItems = document.querySelectorAll('.tab_content_item');
+const tabParent = document.querySelector('.tab_content_items');
+
+let currentIndex = 0;
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach((item) => {
+        item.style.display = 'none';
+    });
+    tabContentItems.forEach((item) => {
+        item.classList.remove('tab_content_item_active');
+    });
+};
+
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block';
+    tabContentItems[index].classList.add('tab_content_item_active');
+};
+
+hideTabContent();
+showTabContent(currentIndex);
+
+tabParent.onclick = (e) => {
+    if (e.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, index) => {
+            if (e.target === item) {
+                hideTabContent();
+                showTabContent(index);
+                currentIndex = index;
+            }
+        });
+    }
+};
+
+setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= tabContentBlocks.length) {
+        currentIndex = 0;
+    }
+    hideTabContent();
+    showTabContent(currentIndex);
+}, 3000);
